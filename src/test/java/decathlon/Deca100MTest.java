6b8bfc3378725a60293f51e7c01692b99b65cad4
Alexander Sandberg <alexandersandbergnystart@gmail.com>
2024-10-02 15:25:38 +0200

    Jag kommer jobba med tester är inte klar

---
+++ src/test/java/decathlon/Deca100MTest.java
@@ -0,0 +1,31 @@
+package decathlon;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+class Deca100MTest {
+
+    @Test
+    void calculateResult() {
+
+        String expected = "";
+        String actual = "";
+        Assertions.assertEquals( expected, actual);
+    }
+
+    @Test
+    void calculateResult2() {
+
+        String expected = "";
+        String actual = "";
+        Assertions.assertEquals( expected, actual);
+    }
+    @Test
+    void calculateResult3() {
+
+        String expected = "";
+        String actual = "";
+        Assertions.assertEquals( expected, actual);
+    }
+}
