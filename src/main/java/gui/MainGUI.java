073863a875c65f71b4f55106d610ddadf62082b7
Staffan Åberg <staffan.aaberg@gmail.com>
2024-10-17 10:39:30 +0200

    Added class Competitor.java to store competitor information and score. Also added a list to actually store the data in MainGUI

--- src/main/java/gui/MainGUI.java
+++ src/main/java/gui/MainGUI.java
@@ -8,7 +8,9 @@ import java.awt.event.ActionListener;
 
 
 import java.awt.*;
+import java.util.ArrayList;
 
+import common.Competitor;
 import decathlon.*;
 
 
@@ -18,7 +20,7 @@ public class MainGUI {
     private JTextField resultField;
     private JComboBox<String> disciplineBox;
     private JTextArea outputArea;
-
+    private ArrayList<Competitor> competitors = new ArrayList<>();
     public static void main(String[] args) {
         new MainGUI().createAndShowGUI();
     }
@@ -118,7 +120,12 @@ public class MainGUI {
                         score = decaShotPut.calculateResult(result);
                         break;
                 }
+                Competitor competitor = new Competitor(name);  // Create a new competitor
+                competitors.add(competitor);        // Add to the list
 
+
+                // Update the competitor's score for the selected discipline
+                competitor.setScore(discipline, score);
                 outputArea.append("Competitor: " + name + "\n");
                 outputArea.append("Discipline: " + discipline + "\n");
                 outputArea.append("Result: " + result + "\n");
